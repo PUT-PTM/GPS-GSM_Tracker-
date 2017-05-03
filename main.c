6d431768065ba1dd332533ee5c6c6522c42b9735
dominpn <dominik.krystkowiak@student.put.poznan.pl>
2017-05-03 15:30:34 +0200

    GPS_IRQ + USART2 Configuration

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 #include "misc.h"
 #include <stdio.h>
 #include <string.h>
-
-char text[36];
-char *split_text[8];
-int i=0;
-uint8_t podglad;
+//https://www.google.com/maps/search/09.1458483,016.5176548/
+int position;
+char gps_char, gps_message[128], time[16], latitude[16], longitude[16];
+uint16_t podglad;
+int IsDigit(int c)
+{
+	return ((c >= '0') && (c <= '9'));
+}
 void GPS_Configuration() //PIN C 10, C 11
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	// wlaczenie taktowania wybranego uk³adu USART
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-	// konfiguracja linii Rx i Tx
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
@@ -23,72 +24,105 @@ void GPS_Configuration() //PIN C 10, C 11
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	// ustawienie funkcji alternatywnej dla pinów (USART)
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
 	USART_InitTypeDef USART_InitStructure;
-	// predkosc transmisji (mozliwe standardowe opcje: 9600, 19200, 38400, 57600, 115200, ...)
 	USART_InitStructure.USART_BaudRate = 9600;
-	// d³ugoœæ s³owa (USART_WordLength_8b lub USART_WordLength_9b)
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	// liczba bitów stopu (USART_StopBits_1, USART_StopBits_0_5, USART_StopBits_2, USART_StopBits_1_5)
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	// sprawdzanie parzystoœci (USART_Parity_No, USART_Parity_Even, USART_Parity_Odd)
 	USART_InitStructure.USART_Parity = USART_Parity_No;
-	// sprzêtowa kontrola przep³ywu (USART_HardwareFlowControl_None, USART_HardwareFlowControl_RTS, USART_HardwareFlowControl_CTS, USART_HardwareFlowControl_RTS_CTS)
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	// tryb nadawania/odbierania (USART_Mode_Rx, USART_Mode_Rx )
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	// konfiguracja
 	USART_Init(USART3, &USART_InitStructure);
-	// wlaczenie ukladu USART
 	USART_Cmd(USART3, ENABLE);
-	//struktura do konfiguracji kontrolera NVIC
 	NVIC_InitTypeDef NVIC_InitStructure;
-	// wlaczenie przerwania zwi¹zanego z odebraniem danych (pozostale zrodla przerwan zdefiniowane sa w pliku stm32f4xx_usart.h)
 	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
 	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	// konfiguracja kontrolera przerwan
 	NVIC_Init(&NVIC_InitStructure);
-	// wlaczenie przerwan od ukladu USART
 	NVIC_EnableIRQ(USART3_IRQn);
 }
-void GPS_Parser(char* message)
+void SIM_Configuration() //PIN A 2, A 3
 {
-	int j=0;
-	char* text = strtok(message, ",");
-	split_text[j]=text;
-	j++;
-	while(text!=0 && j!=7)
-	{
-		text = strtok(0, ",");
-		split_text[j]=text;
-		j++;
-	}
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+	USART_Cmd(USART2, ENABLE);
+	NVIC_InitTypeDef NVIC_InitStructure;
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	NVIC_EnableIRQ(USART2_IRQn);
 }
 void USART3_IRQHandler(void)
 {
-    // sprawdzenie flagi zwiazanej z odebraniem danych przez USART
     if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
     {
-        // odebrany bajt znajduje sie w rejestrze USART3->DR
-    	uint8_t byte = USART_ReceiveData(USART3);
-    	podglad = USART_ReceiveData(USART3);
-    	if(byte=='$' || i>35)
+    	gps_char = USART_ReceiveData(USART3);
+    	if (gps_char == '$')
+    		position = 0;
+    	else if (gps_char == '\r')
     	{
-    		GPS_Parser(text);
-    		i=0;
-    	}
-    	byte = text[i];
-    	i++;
+    		if (!strncmp(gps_message, "GPGGA", 5))
+    	    {
+    			memset(time, 0, sizeof(time));
+    	    	memset(latitude, 0, sizeof(latitude));
+    	    	memset(longitude, 0, sizeof(longitude));
+    	    	strncpy(time, &gps_message[6], 6);
+    	    	if (IsDigit(gps_message[16]))
+    	    	{
+    	    		strncpy(latitude, (const char *)&gps_message[16], 10);
+    	    		latitude[10] = gps_message[27];
+    	    	    strncpy(longitude, (const char *)&gps_message[29], 11);
+    	    	    longitude[11] = gps_message[41];
+    	    	}
+       	     }
+       	}
+      	else
+      		gps_message[position++] = gps_char;
+    }
+}
+void USART2_IRQHandler(void) //TEST
+{
+    // sprawdzenie flagi zwiazanej z odebraniem danych przez USART
+    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+    {
+        // odebrany bajt znajduje sie w rejestrze USART3->DR
+    	uint16_t byte = USART_ReceiveData(USART2);
+    	podglad = USART_ReceiveData(USART2);
+    	//czekaj na opró¿nienie bufora wyjœciowego
+    	while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+    	// wyslanie danych
+    	USART_SendData(USART2, byte);
+    	// czekaj az dane zostana wyslane
+    	while (USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
     }
 }
 int main(void)
 {
 	SystemInit();
+	SIM_Configuration();
 	GPS_Configuration();
     while(1)
     {
